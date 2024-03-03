55542924082d5623d11c89a1434ce9924b2b26b0
Huy Minh Nguyen <minhnguyengp1@gmail.com>
2024-03-03 14:23:27 +0100

    init

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./style.css"
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+
+interface Props {
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList = ({ todos, setTodos } : Props) => {
+  return (
+    // <div className='todos'>
+    //     {todos.map(todo => (
+    //       <SingleTodo 
+    //         todo={todo}
+    //         key={todo.id}
+    //         todos={todos}
+    //         setTodos={setTodos}
+    //       />
+    //     ))}
+    // </div>
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {
+          todos.map(todo => (
+            <SingleTodo 
+              todo={todo}
+              key={todo.id}
+              todos={todos}
+              setTodos={setTodos}
+            />    
+          ))
+        }
+      </div>
+      <div className="todos remove">
+      <span className="todos__heading">Completed Tasks</span>
+        {
+          todos.map(todo => (
+            <SingleTodo 
+              todo={todo}
+              key={todo.id}
+              todos={todos}
+              setTodos={setTodos}
+            />    
+          ))
+        }
+      </div>    
+    </div>
+  )
+}
+
+export default TodoList
